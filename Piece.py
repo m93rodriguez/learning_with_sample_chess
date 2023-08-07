70693c7cf8c8253c4f9303c3c54743fdd3b94edf
Martin Rodriguez-Vega <martrodr@gitlab.inria.fr>
2023-08-08 00:07:14 +0200

    Added png piece sprites, king/rook castling is functional, and en passant logic has been started to be implemented:

--- Piece.py
+++ Piece.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
-import numpy
+import matplotlib.image as mpimg
 import numpy as np
 
 
 class Piece:
+    type = 'Pawn'
+
     def __init__(self, coords, team):
         self.position = coords
         self.team = team
@@ -11,19 +13,17 @@ class Piece:
         self.visual = self.plot()
 
     def plot(self):
-        if self.team == 1:
-            line_spec = 'r'
-        else:
-            line_spec = 'b'
-
-        return plt.text(self.position[0], self.position[1], 'p', color=line_spec,
-                        horizontalalignment='center', verticalalignment='center')
+        color_list = ['White', 'Black']
+        icon_file = 'icons/' + self.type + '_' + color_list[self.team] + '.png'
+        icon_img = mpimg.imread(icon_file)
+        img_pos = (-0.5+self.position[0], 0.5+self.position[0], -0.5+self.position[1], 0.5+self.position[1])
+        return plt.imshow(icon_img, extent=img_pos, alpha=1)
 
     def move(self, new_position):
         self.position = new_position
         self.has_moved = True
-        self.visual.set_x(new_position[0])
-        self.visual.set_y(new_position[1])
+        img_pos = (-0.5 + new_position[0], 0.5 + new_position[0], -0.5 + new_position[1], 0.5 + new_position[1])
+        self.visual.set_extent(img_pos)
 
     def gets_eaten(self):
         self.visual.remove()
@@ -47,73 +47,92 @@ class Pawn(Piece):
         self.type = 'Pawn'
         self.y_step = y_step
         Piece.__init__(self, coords, team)
-        self.visual.set_text('P')
 
     def attack_cases(self, allies, foes):
-        reachable = []
+        reachable = np.empty((0, 2))
         for direction in [-1, 1]:
             test_pos = self.position + np.array([direction, self.y_step])
             if (test_pos == foes).all(1).any():
-                reachable.append(test_pos)
-        return np.array(reachable)
+                reachable = np.append(reachable, [test_pos], axis=0)
+        return reachable
 
-    def movement_range(self, allies, foes):
+    def attack_en_passant(self, ally, foe):
+        reachable = np.empty((0, 2))
+        if foe.prev_turn_jump is None:
+            return reachable
+        attack_row = 7 - ally.back_row - 3*self.y_step
+        if not self.position[1] == attack_row:
+            return reachable
+        if np.abs(foe.prev_turn_jump[0] - self.position[0]) < 2:
+            reachable = foe.prev_turn_jump
+            reachable[1] = reachable[1] + self.y_step
+        return np.array([reachable])
 
-        reachable = []
+    def movement_range(self, ally, foe, **kwargs):
+        
+        ally_pos = ally.piece_positions
+        foe_pos = foe.piece_positions
+        reachable = np.empty((0, 2))
 
         for mag in [1, 2]:
             forward_move = self.position + np.array([0, mag*self.y_step])
 
-            if ((forward_move == allies).all(1).any() or (forward_move == foes).all(1).any() or
+            if ((forward_move == ally_pos).all(1).any() or (forward_move == foe_pos).all(1).any() or
                     not self.is_inside_board(forward_move)):
                 break
-            reachable.append(forward_move)
+            reachable = np.append(reachable, [forward_move], axis=0)
             if self.has_moved:
                 break
-        reachable = np.array(reachable)
-        side_move = self.attack_cases(allies, foes)
+        side_move = self.attack_cases(ally_pos, foe_pos)
+        side_move = np.append(side_move, self.attack_en_passant(ally, foe), axis=0)
 
         if reachable.size == 0:
             return side_move
 
         if side_move.size == 0:
             return reachable
-        else:
-            return np.append(reachable, side_move, axis=0)
+        
+        return np.append(reachable, side_move, axis=0)
+
+    def move(self, new_position):
+        old_position = self.position
+        super().move(new_position)
+        if np.abs(old_position[1] - new_position[1]) > 1:
+            return 'Pawn_Jump'
 
 
 class Rook(Piece):
     def __init__(self, coords, team):
         self.type = 'Rook'
         Piece.__init__(self, coords, team)
-        self.visual.set_text('R')
 
     @classmethod
     def attack_cases(cls, position, allies, foes, max_movement=7):
-        reachable = []
+        reachable = np.empty((0, 2))
         for direction in [[0, 1], [1, 0]]:
             for sense in [1, -1]:
                 for mag in range(1, max_movement+1):
-                    test_pos = position + np.array(direction)*sense*mag
+                    test_pos = position + np.array([direction])*sense*mag
                     if not cls.is_inside_board(test_pos):
                         break
                     if (test_pos == allies).all(1).any():
                         break
-                    reachable.append(test_pos)
+                    reachable = np.append(reachable, test_pos, axis=0)
                     if (test_pos == foes).all(1).any():
                         break
 
         return np.array(reachable)
 
-    def movement_range(self, allies, foes):
-        return self.attack_cases(self.position, allies, foes)
+    def movement_range(self, ally, foe, **kwargs):
+        ally_pos = ally.piece_positions
+        foe_pos = ally.piece_positions
+        return self.attack_cases(self.position, ally_pos, foe_pos)
 
 
 class Knight(Piece):
     def __init__(self, coords, team):
         self.type = 'Knight'
         Piece.__init__(self, coords, team)
-        self.visual.set_text('H')
 
     def attack_cases(self, allies, foes):
         reachable = []
@@ -130,46 +149,52 @@ class Knight(Piece):
 
         return np.array(reachable)
 
-    def movement_range(self, allies, foes):
-        return self.attack_cases(allies, foes)
+    def movement_range(self, ally, foe, **kwargs):
+        ally_pos = ally.piece_positions
+        foe_pos = foe.piece_positions
+        return self.attack_cases(ally_pos, foe_pos)
 
 
 class Bishop(Piece):
     def __init__(self, coords, team):
         self.type = 'Bishop'
         Piece.__init__(self, coords, team)
-        self.visual.set_text('B')
 
     @classmethod
     def attack_cases(cls, position, allies, foes, max_movement=7):
-        reachable = []
+        reachable = np.empty((0, 2))
         for x_dir in [-1, 1]:
             for y_dir in [-1, 1]:
                 for mag in range(1, max_movement+1):
-                    test_pos = position + np.array([x_dir, y_dir]) * mag
+                    test_pos = position + np.array([[x_dir, y_dir]]) * mag
                     if not cls.is_inside_board(test_pos):
                         break
                     if (test_pos == allies).all(1).any():
                         break
-                    reachable.append(test_pos)
+
+                    reachable = np.append(reachable, test_pos, axis=0)
+
                     if (test_pos == foes).all(1).any():
                         break
 
         return np.array(reachable)
 
-    def movement_range(self, allies, foes):
-        return self.attack_cases(self.position, allies, foes)
+    def movement_range(self, ally, foe, **kwargs):
+        ally_pos = ally.piece_positions
+        foe_pos = foe.piece_positions
+        return self.attack_cases(self.position, ally_pos, foe_pos)
 
 
 class Queen(Piece):
     def __init__(self, coords, team):
         self.type = 'Queen'
         Piece.__init__(self, coords, team)
-        self.visual.set_text('Q')
 
-    def movement_range(self, allies, foes):
-        reachable_rook = Rook.attack_cases(self.position, allies, foes)
-        reachable_bishop = Bishop.attack_cases(self.position, allies, foes)
+    def movement_range(self, ally, foe, **kwargs):
+        ally_pos = ally.piece_positions
+        foe_pos = foe.piece_positions
+        reachable_rook = Rook.attack_cases(self.position, ally_pos, foe_pos)
+        reachable_bishop = Bishop.attack_cases(self.position, ally_pos, foe_pos)
         if reachable_rook.size == 0:
             return reachable_bishop
         if reachable_bishop.size == 0:
@@ -181,26 +206,91 @@ class King(Piece):
     def __init__(self, coords, team):
         self.type = 'King'
         Piece.__init__(self, coords, team)
-        self.visual.set_text('K')
 
-    def movement_range(self, allies, foes):
-        return np.append(Rook.attack_cases(self.position, allies, foes, max_movement=1),
-                         Bishop.attack_cases(self.position, allies, foes, max_movement=1), axis=0)
+    def is_in_check(self, ally, foe):
+        if any(np.all(self.position == foe.attacked_cases(ally), axis=1)):
+            return True
+        return False
+
+    def castling_moves(self, ally, foe):
+        if self.has_moved:
+            return np.empty((0, 2))
+
+        if self.is_in_check(ally, foe):
+            return np.empty((0, 2))
+
+        attacked = foe.attacked_cases(ally)
+        reachable = np.empty((0, 2))
+        for rook in ally.Pieces['Rook']:
+            if rook.has_moved:
+                continue
+            if not self.position[1] == rook.position[1]:
+                continue
+            possible = True
+            start = np.minimum(self.position[0], rook.position[0]) + 1
+            stop = np.maximum(self.position[0], rook.position[0])
+            for x in range(start, stop):
+                case = np.array([[x, self.position[1]]])
+                if (any(np.all(case == attacked, axis=1)) or any(np.all(case == ally.piece_positions, axis=1)) or
+                        any(np.all(case == foe.piece_positions, axis=1))):
+                    possible = False
+                    break
+            if possible:
+                direction = np.sign(rook.position[0] - self.position[0])
+                reachable = np.append(reachable, np.array([[self.position[0] + 2*direction, self.position[1]]]), axis=0)
+
+        return reachable
+
+    def movement_range(self, ally, foe, **kwargs):
+        # kwargs:
+        #   castling (= True): if True, calculates the possible moves due to castling with rooks
+
+        ally_pos = ally.piece_positions
+        foe_pos = foe.piece_positions
+        reachable_rook = Rook.attack_cases(self.position, ally_pos, foe_pos, max_movement=1)
+        reachable_bishop = Bishop.attack_cases(self.position, ally_pos, foe_pos, max_movement=1)
+        reachable = np.append(reachable_rook, reachable_bishop, axis=0)
+
+        if 'castling' not in kwargs.keys():
+            return np.append(reachable, self.castling_moves(ally, foe), axis=0)
+
+        if kwargs['castling']:
+            return np.append(reachable, self.castling_moves(ally, foe), axis=0)
+
+        return reachable
+
+    def move(self, new_position):
+        old_position = self.position
+        super().move(new_position)
+        if np.abs(old_position[0] - new_position[0]) > 1:
+            return 'Castled'
 
 
 class Team:
-    def __init__(self, back_row, front_row, team_index):
-        self.back_row = back_row
+    def __init__(self, team_index):
+        step_list = [-1, 1]
+        back_row_list = [7, 0]
+        self.back_row = back_row_list[team_index]
+        self.prev_turn_jump = None
         self.index = team_index
         self.Pieces = {
-            'Pawn': [Pawn(coords=np.array([cnt, front_row]), team=team_index, y_step=team_index) for cnt in range(8)],
-            'Rook': [Rook(coords=np.array([cnt, back_row]), team=team_index) for cnt in [0, 7]],
-            'Knight': [Knight(coords=np.array([cnt, back_row]), team=team_index) for cnt in [1, 6]],
-            'Bishop': [Bishop(coords=np.array([cnt, back_row]), team=team_index) for cnt in [2, 5]],
-            'Queen': [Queen(coords=np.array([cnt, back_row]), team=team_index) for cnt in [4]],
-            'King': [King(coords=np.array([cnt, back_row]), team=team_index) for cnt in [3]],
+            'Pawn': [Pawn(coords=np.array([cnt, self.back_row+step_list[team_index]]),
+                          team=team_index, y_step=step_list[team_index]) for cnt in range(8)],
+            'Rook': [Rook(coords=np.array([cnt, self.back_row]), team=team_index) for cnt in [0, 7]],
+            'Knight': [Knight(coords=np.array([cnt, self.back_row]), team=team_index) for cnt in [1, 6]],
+            'Bishop': [Bishop(coords=np.array([cnt, self.back_row]), team=team_index) for cnt in [2, 5]],
+            'Queen': [Queen(coords=np.array([cnt, self.back_row]), team=team_index) for cnt in [4]],
+            'King': [King(coords=np.array([cnt, self.back_row]), team=team_index) for cnt in [3]],
         }
         (self.piece_positions, self.piece_list) = self.list_pieces()
+
+    def attacked_cases(self, foe):
+        team_moves = np.empty((0, 2))
+        for piece_iter in self.piece_list:
+            piece_moves = self.Pieces[piece_iter[0]][piece_iter[1]].movement_range(self, foe, castling=False)
+            if piece_moves.size > 1:
+                team_moves = np.append(team_moves, piece_moves, axis=0)
+        return team_moves
 
     def list_pieces(self):
 
@@ -214,16 +304,48 @@ class Team:
                 current_piece = current_piece + 1
         return np.array(piece_positions), piece_id
 
+    def find_piece(self, position):
+        if self.piece_positions.size > 0:
+            selected = np.all(self.piece_positions == position, axis=1)
+            if any(selected):
+                selected = np.nonzero(selected)
+                return selected[0][0]  # Output of nonzero is tuple for each dimension
+
     def move_piece(self, piece_ind, position, foe):
+        self.prev_turn_jump = None
+
+        piece = self.Pieces[self.piece_list[piece_ind][0]][self.piece_list[piece_ind][1]]
+
+        foe.eat_piece(foe.find_piece(position))
+
         if foe.piece_positions.size > 0:
             conflict_piece = (foe.piece_positions == position).all(1)
             if any(conflict_piece):
-                eaten_piece_ind = numpy.nonzero(conflict_piece)
+                eaten_piece_ind = np.nonzero(conflict_piece)
                 foe.eat_piece(eaten_piece_ind[0][0])
-        self.Pieces[self.piece_list[piece_ind][0]][self.piece_list[piece_ind][1]].move(position)
+        move_state = piece.move(position)
+
+        if move_state == 'Castled':
+            dist = np.inf
+            rook_id = 0
+            cont = 0
+            for rook in self.Pieces['Rook']:
+                if dist > np.abs(piece.position[0] - rook.position[0]):
+                    rook_id = cont
+                    dist = np.abs(piece.position[0] - rook.position[0])
+                cont = cont + 1
+            rook = self.Pieces['Rook'][rook_id]
+            direction = np.sign(piece.position[0] - rook.position[0])
+            rook.move([piece.position[0]+direction, piece.position[1]])
+
+        if move_state == 'Pawn_Jump':
+            self.prev_turn_jump = position
+
         self.piece_positions = self.list_pieces()[0]
 
     def eat_piece(self, piece_ind):
+        if piece_ind is None:
+            return
         self.Pieces[self.piece_list[piece_ind][0]][self.piece_list[piece_ind][1]].gets_eaten()
         del self.Pieces[self.piece_list[piece_ind][0]][self.piece_list[piece_ind][1]]
         self.piece_positions, self.piece_list = self.list_pieces()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import numpy as np
 import time
 from Piece import Pawn, Rook, Knight, Bishop, Queen, King, Team
@@ -14,20 +15,25 @@ for row in range(board_size):
 
 fig, ax = plt.subplots()
 
-plt.imshow(board_color, cmap='Greys')
+plt.imshow(board_color, cmap='Greys', extent=(-.5, 7.5, -.5, 7.5), alpha=0.5)
 
-allowed_moves_visual = plt.plot([], [], 'yo')
+player = [Team(0), Team(1)]
 
-player = [Team(0, 1, 1), Team(7, 6, -1)]
-
-plt.ioff()
-
-player[1].move_piece(0, [3, 2], player[0])
+#player[1].move_piece(0, [3, 2], player[0])
 
 turn = 0
 turn_starts = True
 piece_id = None
 possible_moves = None
+
+allowed_moves_visual = plt.plot([], [], 'yo')
+
+ac = player[0].attacked_cases(player[1])
+
+
+ax = plt.gca()
+ax.set_xlim([-0.5, 7.5])
+ax.set_ylim([-0.5, 7.5])
 
 
 def onclick(event):
@@ -37,32 +43,31 @@ def onclick(event):
 
         _piece_id = (player[turn].piece_positions == position).all(1)
         if not any(_piece_id):
-            return None, None
+            return None, None  # Returns both None if enemy piece or empty case is selected
         _piece_id = np.argwhere(_piece_id)[0, 0]
 
         current_piece = player[turn].Pieces[player[turn].piece_list[_piece_id][0]]
         current_piece = current_piece[player[turn].piece_list[_piece_id][1]]
-        _possible_moves = current_piece.movement_range(player[turn].piece_positions,
-                                                      player[(turn+1) % 2].piece_positions)
+        _possible_moves = current_piece.movement_range(player[turn], player[(turn+1) % 2])
 
         if _possible_moves.size < 2:
             allowed_moves_visual[0].set_xdata([])
             allowed_moves_visual[0].set_ydata([])
-            return _piece_id, None
+            return _piece_id, None  # Returns one number and one None if no movable places are possible
         else:
             allowed_moves_visual[0].set_xdata(_possible_moves[:, 0])
             allowed_moves_visual[0].set_ydata(_possible_moves[:, 1])
             
-        return _piece_id, _possible_moves
+        return _piece_id, _possible_moves  # Returns chosen piece and possible moves if both exist
 
     def select_destination():
         if any((position == player[turn].piece_positions).all(1)):
-            return select_own_piece()
+            return select_own_piece()  # If an allied piece is selected, behave as if turn is starting
         if any((position == possible_moves).all(1)):
             player[turn].move_piece(piece_id, position, player[(turn+1) % 2])
-            return None, None
+            return None, None  # If movable space is selected, move the piece and "clean" the output variables
         else:
-            return piece_id, possible_moves
+            return piece_id, possible_moves  # If no suitable move is chosen, nothing changes and wait for next call
 
     position = [np.round(event.xdata), np.round(event.ydata)]
 
@@ -72,7 +77,7 @@ def onclick(event):
             turn_starts = False
     else:
         piece_id, possible_moves = select_destination()
-        if piece_id is None:
+        if piece_id is None:  # This happens if select_destination() succeed in finding movable case
             turn = (turn + 1) % 2
             turn_starts = True
             allowed_moves_visual[0].set_xdata([])
@@ -80,10 +85,14 @@ def onclick(event):
 
     # player[turn].move_piece(0, position, player[1])
     plt.draw()
-    # print(player[1].piece_positions.size)
 
 
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
+
+print(ac)
+
+allowed_moves_visual[0].set_xdata(ac[:, 0])
+allowed_moves_visual[0].set_ydata(ac[:, 1])
 
 plt.show()
 
